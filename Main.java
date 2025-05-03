7be143624874bea072062b66f9c0389fd2d21924
Aleksandr Perov <aleksperdev@gmail.com>
2025-05-04 01:26:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> firstClients = List.of("Anya", "Sveta", "Olya", "Alexandra",
+                "Ruslana", "Olesya", "Vika");
+
+        Queue<String> queue = new LinkedList<>(firstClients);
+
+        while (!queue.isEmpty()) {
+            String name = queue.poll();
+            System.out.println(name + " сделал новый маникюр");
+
+            if (Math.random() < 0.5) {
+                queue.offer("a friend of " + name);
+            }
+        }
+    }
+}
+
+
